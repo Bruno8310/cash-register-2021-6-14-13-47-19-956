ff4b64fdbc39ac98f0a44831a9f53f773f095be1
GONGBR2 <gongbr2@oocl.com>
2021-06-16 18:45:12 +0800

    [CashRegister] feature: use mocktio framework to verify that cashRegister.process will trigger print.

--- src/test/java/com/tw/academy/CashRegisterTest.java
+++ src/test/java/com/tw/academy/CashRegisterTest.java
@@ -2,7 +2,14 @@ package com.tw.academy;
 
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+
 class CashRegisterTest {
+
+	private final String TEST = "test";
+
 	@Test
 	void should_process_execute_printing() {
 		//given
@@ -13,4 +20,19 @@ class CashRegisterTest {
 		//then
 		//verify that cashRegister.process will trigger print
 	}
+
+	@Test
+	void should_verify_trigger_print_method_when_call_process_method() {
+		// given
+		Printer printer = spy(Printer.class);
+		Purchase purchase = mock(Purchase.class);
+		CashRegister cashRegister = new CashRegister(printer);
+//		when(purchase.asString()).thenReturn(TEST);
+		given(purchase.asString()).willReturn(TEST);
+		// when
+		cashRegister.process(purchase);
+		// then
+		verify(printer, times(1)).print(TEST);
+	}
+
 }
